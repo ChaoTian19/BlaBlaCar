10e901c8f2bcaff10faa66326393e772d5d18082
ChaoTian <54988298+ChaoTian19@users.noreply.github.com>
2019-10-10 21:27:48 +0800

    update

--- app/src/main/java/com/jrteamtech/clonebla/activity/DropoffActivity.java
+++ app/src/main/java/com/jrteamtech/clonebla/activity/DropoffActivity.java
@@ -9,8 +9,6 @@ import com.jrteamtech.clonebla.R;
 
 public class DropoffActivity extends AppCompatActivity {
 
-   
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
