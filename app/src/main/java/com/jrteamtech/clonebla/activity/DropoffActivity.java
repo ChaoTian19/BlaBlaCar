5c393016d87b017d440b295f5c0e5422aec1f163
ChaoTian <54988298+ChaoTian19@users.noreply.github.com>
2019-10-10 21:19:16 +0800

    update

--- app/src/main/java/com/jrteamtech/clonebla/activity/DropoffActivity.java
+++ app/src/main/java/com/jrteamtech/clonebla/activity/DropoffActivity.java
@@ -3,14 +3,19 @@ package com.jrteamtech.clonebla.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.jrteamtech.clonebla.R;
 
 public class DropoffActivity extends AppCompatActivity {
 
+    private Button ddd;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dropoff);
+
+
     }
 }
