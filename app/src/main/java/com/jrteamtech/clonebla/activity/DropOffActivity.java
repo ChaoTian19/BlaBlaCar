c78fa4bf5e6af34b6a019155467f496dba3007e7
iartview <55128670+iartview@users.noreply.github.com>
2019-10-10 21:23:28 +0800

    update

--- app/src/main/java/com/jrteamtech/clonebla/activity/DropOffActivity.java
+++ app/src/main/java/com/jrteamtech/clonebla/activity/DropOffActivity.java
@@ -1,16 +1,18 @@
 package com.jrteamtech.clonebla.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.jrteamtech.clonebla.R;
 
-public class StopoversActivity extends AppCompatActivity {
+public class DropOffActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_stopovers);
+        setContentView(R.layout.activity_dropoff);
+
+
     }
 }
