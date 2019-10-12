27ba75c40293cb2d4280b229c50163be6cbcd916
iartview <55128670+iartview@users.noreply.github.com>
2019-10-13 01:28:25 +0800

    update

--- app/src/main/java/com/jrteamtech/clonebla/activity/PickUpActivity.java
+++ app/src/main/java/com/jrteamtech/clonebla/activity/PickUpActivity.java
@@ -20,15 +20,15 @@ public class PickUpActivity extends AppCompatActivity implements View.OnClickLis
     @RequiresApi(api = 21)
     public void onCreate(Bundle bundle) {
         super.onCreate(bundle);
-        setContentView((int) R.layout.activity_pickup);
+        setContentView( R.layout.activity_pickup);
         getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.white));
         initid();
         this.edtLocationSearch.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             public void onFocusChange(View view, boolean z) {
                 if (z) {
-                    Intent intent = new Intent(PickUpActivity.this.getApplicationContext(), SearchActivity.class);
-                    intent.putExtra(String.valueOf(R.string.activity_name), PickUpActivity.class.getSimpleName());
-                    PickUpActivity.this.startActivity(intent);
+//                    Intent intent = new Intent(PickUpActivity.this.getApplicationContext(), SearchActivity.class);
+//                    intent.putExtra(String.valueOf(R.string.activity_name), PickUpActivity.class.getSimpleName());
+//                    PickUpActivity.this.startActivity(intent);
                 }
             }
         });
@@ -45,7 +45,7 @@ public class PickUpActivity extends AppCompatActivity implements View.OnClickLis
         int id = view.getId();
         if (id == R.id.edt_location_search) {
             Intent intent = new Intent(getApplicationContext(), SearchActivity.class);
-            intent.putExtra(String.valueOf(R.string.activity_name), PickUpActivity.class.getSimpleName());
+            intent.putExtra(getResources().getString(R.string.activity_name), PickUpActivity.class.getSimpleName());
             startActivity(intent);
         } else if (id == R.id.img_back_arrow) {
             finish();
